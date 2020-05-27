371d371b82e01a63a71b3e033dd29ca0e66c3040
Ajay S <ajay_sieunarine@hotmail.com>
2020-05-26 22:26:07 -0400

    added camera system

---
+++ game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // player
+    public float smoothSpeed = 0.125f; // make it smooth
+    public Vector3 offset = new Vector3(0, 6, -10);
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        // Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        // transform.position = smoothedPos;
+        transform.position = desiredPos;
+    }
+
+}
